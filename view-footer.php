130b7a929a7cfafbfd2b97ff826f1a0be70aebd8
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-11-27 14:15:27 -0600

    Update view-footer.php

--- view-footer.php
+++ view-footer.php
@@ -1,5 +1,5 @@
- </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-    <span class="mb-3 mb-md-0 text-muted">Created By: Abby, Ashley, and Nicole</span>
-  </body>
+    <span class="mb-3 mb-md-0 text-muted">Created by: Abby, Ashley, and Nicole</span>
+   </div>
+</body>
 </html>
