4007950db4fedd7f258b6454655bfd338dfd5be8
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-11-27 14:16:05 -0600

    Create countries.php

---
+++ countries.php
@@ -0,0 +1,35 @@
+<div id="carouselExampleCaptions" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Spain"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="England"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Germany"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="spain.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block"
+        <p>Home to 6 leagues</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="england.jpeg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <p>Home to 57 leagues</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="germany.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <p>Home to 57 leagues</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
