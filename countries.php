3dd690606ee92faba9e3b75dd1c43504a7bd0b31
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-11-27 14:33:16 -0600

    Update countries.php

--- countries.php
+++ countries.php
@@ -2,27 +2,30 @@
   <body>
   <div id="carouselExampleCaptions" class="carousel slide">
   <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Spain"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="England"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Germany"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img src="spain.png" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block"
-        <p>Home to 6 leagues</p>
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Spain</h5>
+        <p>Home to 6 leagues.</p>
       </div>
     </div>
     <div class="carousel-item">
       <img src="england.jpeg" class="d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
-        <p>Home to 57 leagues</p>
+        <h5>England</h5>
+        <p>Home to 57 leagues.</p>
       </div>
     </div>
     <div class="carousel-item">
       <img src="germany.png" class="d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
-        <p>Home to 57 leagues</p>
+        <h5>Germany</h5>
+        <p>Home to 31 leagues.</p>
       </div>
     </div>
   </div>
@@ -34,6 +37,6 @@
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
-</div>
+</div>     
   </body>
 </html>
