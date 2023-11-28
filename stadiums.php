449438c38a0ab53cf366d53b297631852f87b34e
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-11-28 16:41:31 -0600

    Update stadiums.php

--- stadiums.php
+++ stadiums.php
@@ -24,7 +24,7 @@ echo '<div class="alert alert-danger" role="alert">Error</div>"';
     }
   break;
   case "Edit":
-    if(updateStadium($_POST['sName'], $_POST['sLat'], $_POST['sLong'])) {
+    if(updateStadium($_POST['sid'],$_POST['sName'], $_POST['sLat'], $_POST['sLong'])) {
   echo '<div style="margin:15px" class="alert alert-success" role="alert">Stadium Edited</div>';
     }
     else{
