c01057c1c631a3c73aaf34ee5d6dcbf65883507b
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-11-28 16:20:36 -0600

    Update model-stadiums.php

--- model-stadiums.php
+++ model-stadiums.php
@@ -12,4 +12,47 @@ function selectStadiums() {
         throw $e;
     }
 }
+
+function insertStadium($sName, $sLong, $sLat) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `stadium` ( `stadium_name`, `stadium_long`, `stadium_lat`) VALUES (?, ?, ?)");
+        $stmt->bind_param("sss", $sName, $sLong, $sLat);
+        $success=$stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateStadium($sid, $sName, $sLat, $sLong) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `stadium` set `stadium_name` = ?, `stadium_long` = ?, `stadium_lat` = ? where stadium_id = ?");
+        $stmt->bind_param("sssi", $sName, $sLong, $sLat, $sid);
+        $success=$stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteStadium($sid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from `stadium` where stadium_id = ?");
+        $stmt->bind_param("i", $sid);
+        $success=$stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
 ?>
