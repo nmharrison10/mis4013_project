70dd49a85b4fa53df28f2de2c88fffd0ba7dac6e
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-11-28 16:38:26 -0600

    Update view-stadiums.php

--- view-stadiums.php
+++ view-stadiums.php
@@ -3,14 +3,10 @@
 <h1>Stadiums</h1>
   </div>
   <div class="col-auto">
-<?php
-include "view-stadiums-newform.php";
-?>
   </div>
 </div>
 <link href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.3/leaflet.min.css" rel="stylesheet"/>              
 <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.3/leaflet.min.js"></script>
-
 <div class="table-responsive">
   <table class="table">
     <head>
