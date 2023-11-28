18b48f0becc13ad44771f3bdce1ff5176fdd4941
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-11-28 16:24:39 -0600

    Update stadiums.php

--- stadiums.php
+++ stadiums.php
@@ -4,6 +4,36 @@ require_once("model-stadiums.php");
 
 $pageTitle = "Stadiums";
 include "view-header.php";
+
+if (isset($_POST['actionType'])){
+switch($_POST['actionType']){
+  case "Add":
+    if(insertStadium($_POST['sName'], $_POST['sLong']), $_POST['sLat']) ) {
+  echo '<div style="margin:15px" class="alert alert-success" role="alert">Stadium Added</div>';
+    }
+    else{
+echo '<div class="alert alert-danger" role="alert">Error</div>"';
+    }
+  break;
+  case "Delete":
+    if(deleteStadium($_POST['sid'])) {
+  echo '<div style="margin:15px" class="alert alert-success" role="alert">Stadium Deleted</div>';
+    }
+    else{
+echo '<div class="alert alert-danger" role="alert">Error</div>"';
+    }
+  break;
+  case "Edit":
+    if(updateStadium($_POST['sName'], $_POST['sLat'], $_POST['sLong'])) {
+  echo '<div style="margin:15px" class="alert alert-success" role="alert">Stadium Edited</div>';
+    }
+    else{
+echo '<div class="alert alert-danger" role="alert">Error</div>"';
+    }
+  break;
+}
+}
+
 $stadiums = selectStadiums();
 include "view-stadiums.php";
 include "view-footer.php";
