9d7372ba6e17ec2f66f0e81d3ab6c468c8402a37
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-11-28 20:18:02 -0600

    Update view-stadiums-editform.php

--- view-stadiums-editform.php
+++ view-stadiums-editform.php
@@ -51,10 +51,10 @@
 "use strict";
 var mapedit = L.map( 'mapdiv', { attributionControl: false, } ).setView( [ 35.33443889141701, -97.07270547900498 ], 12 );
 
-L.tileLayer( 'https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: ''
-} ).addTo( mapedit );
+let googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
+    maxZoom: 20,
+    subdomains:['mt0','mt1','mt2','mt3']
+}).addTo( mapedit );
 
 
 </script>
