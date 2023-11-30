b119437ce2c45cca3454005f75060c0c4cddd016
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-11-29 18:55:51 -0600

    Update countries.php

--- countries.php
+++ countries.php
@@ -1,10 +1,10 @@
-    <?php
+<?php
 require_once("util-db.php");
-//require_once("countries.php");
+require_once("model-countries.php");
 
 $pageTitle = "Countries";
 include "view-header.php";
-?>
+
   <div id="carouselExampleCaptions" class="carousel slide">
   <div class="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -43,9 +43,39 @@ include "view-header.php";
     <span class="visually-hidden">Next</span>
   </button>
 </div>    
-<?php
-   // $countries = selectCountries();
-//include "view-stadiums.php";
+
+if (isset($_POST['actionType'])){
+switch($_POST['actionType']){
+  case "Add":
+    if(insertCountry($_POST['cName'], $_POST['capName'])) {
+  echo '<div style="margin:15px" class="alert alert-success" role="alert">Country Added</div>';
+    }
+    else{
+echo '<div class="alert alert-danger" role="alert">Error</div>"';
+    }
+  break;
+  case "Delete":
+    if(deleteCountry($_POST['cid'])) {
+  echo '<div style="margin:15px" class="alert alert-success" role="alert">Country Deleted</div>';
+    }
+    else{
+echo '<div class="alert alert-danger" role="alert">Error</div>"';
+    }
+  break;
+  case "Edit":
+    if(updateCountry($_POST['cName'], $_POST['capName'], $_POST['cid'])) {
+  echo '<div style="margin:15px" class="alert alert-success" role="alert">Country Edited</div>';
+    }
+    else{
+echo '<div class="alert alert-danger" role="alert">Error</div>"';
+    }
+  break;
+}
+}
+
+$countries = selectCountries();
+include "view-countries.php";
 include "view-footer.php";
 ?>
+
 
