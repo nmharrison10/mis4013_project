be3fe5268f78876a7cf0dc3e150a06f246343190
AshleyMeza <111796979+AshleyMeza@users.noreply.github.com>
2023-12-01 14:54:20 -0600

    Create view-players-newform.php

---
+++ view-players-newform.php
@@ -0,0 +1,33 @@
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newPlayerModal">
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+  </svg>
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="newPlayerModal" tabindex="-1" aria-labelledby="newPlayerModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="newPlayerModalLabel">New Player</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form method = "post" action="">
+            <div class="mb-3">
+              <label for="player_id" class="form-label">Player's Name</label>
+              <input type="text" class="form-control" id="player_id" name = "player_id">
+      </div>
+            <div class="mb-3">
+              <label for="player_age" class="form-label">Player's Age</label>
+              <input type="text" class="form-control" id="player_age" name = "player_age">
+            </div>
+                <input type = "hidden" name = "actionType" value = "Add"> 
+            <button type="submit" class="btn btn-primary">Save</button>
+          </form>
+      </div>
+    </div>
+  </div>
+</div>
