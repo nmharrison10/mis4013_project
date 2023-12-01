1ef1cfae21cb148203606071f7b495f36412ec3e
AshleyMeza <111796979+AshleyMeza@users.noreply.github.com>
2023-12-01 15:16:20 -0600

    Update view-players-newform.php

--- view-players-newform.php
+++ view-players-newform.php
@@ -11,18 +11,18 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="newPlayerModalLabel">New Player</h1>
+        <h1 class="modal-title fs-5" id="newPlayerModalLabel">Add a New Player</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
         <form method = "post" action="">
             <div class="mb-3">
-              <label for="player_id" class="form-label">Player's Name</label>
-              <input type="text" class="form-control" id="player_id" name = "player_id">
+              <label for="pName" class="form-label">Player's Name</label>
+              <input type="text" class="form-control" id="pName" name = "pName">
       </div>
             <div class="mb-3">
-              <label for="player_age" class="form-label">Player's Age</label>
-              <input type="text" class="form-control" id="player_age" name = "player_age">
+              <label for="pAge" class="form-label">Player's Current Age</label>
+              <input type="text" class="form-control" id="pAge" name = "pAge">
             </div>
                 <input type = "hidden" name = "actionType" value = "Add"> 
             <button type="submit" class="btn btn-primary">Save</button>
