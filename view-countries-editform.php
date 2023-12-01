ec8725ae9945214c8552abadfbc7c0fb7e52e84d
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-12-01 14:49:46 -0600

    Update view-countries-editform.php

--- view-countries-editform.php
+++ view-countries-editform.php
@@ -1,11 +1,3 @@
-<!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editCountryModal<?php echo $country['country_id'];?>">
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-      <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-      <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-    </svg>
-</button>
-
 <!-- Bootstrap assets -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
@@ -16,30 +8,21 @@
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.1/dist/leaflet.css" integrity="sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ==" crossorigin="" />
 <script src="https://unpkg.com/leaflet@1.3.1/dist/leaflet-src.js" integrity="sha512-IkGU/uDhB9u9F8k+2OsA6XXoowIhOuQL1NTgNZHY1nkURnqEGlDZq3GsfmdJdKFe1k1zOc6YU2K7qY+hF9AodA==" crossorigin=""></script>
 
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+  Launch demo modal that contains the map container.
+</button>
+
 <!-- Modal -->
-<div class="modal fade" id="editCountryModal<?php echo $country['country_id'];?>" tabindex="-1" aria-labelledby="editCountryModalLabel<?php echo $country['country_id'];?>" aria-hidden="true">
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h1 class="modal-title fs-5" id="editCountryModalLabel<?php echo $country['country_id'];?>">Edit Country</h1>
+        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
       </div>
       <div class="modal-body">
 
-          <form method="post" action="">
-          <div class="mb-3">
-            <label for="cName<?php echo $country['country_id'];?>" class="form-label">Country Name</label>
-            <input type="text" class="form-control" id="cName<?php echo $country['country_id'];?>" name="cName" value="<?php echo $country['country_name'];?>">
-          </div>
-          <div class="mb-3">
-            <label for="capName<?php echo $country['country_id'];?>" class="form-label">Country Capital</label>
-            <input type="text" class="form-control" id="capName<?php echo $country['country_id'];?>" name="capName" value="<?php echo $country['capital'];?>">
-          </div>
-              <input type="hidden" name="cid" value="<?php echo $country['country_id'];?>">
-          <input type="hidden" name="actionType" value="Edit">
-          <button type="submit" class="btn btn-primary">Save</button>
-            </form>
-          
         <!-- map container -->
         <div id="map" style="height: 180px"></div>
 
@@ -61,7 +44,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // Comment out the below code to see the difference.
-$('#editCountryModal<?php echo $country['country_id'];?>').on('shown.bs.modal', function() {
+$('#myModal').on('shown.bs.modal', function() {
   map.invalidateSize();
 });
 </script>
