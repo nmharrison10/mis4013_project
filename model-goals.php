71a75247e42242d6957655b39a8a7610e2116a2c
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-12-04 12:53:16 -0600

    Update model-goals.php

--- model-goals.php
+++ model-goals.php
@@ -2,7 +2,7 @@
 function selectGoals() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT country_id, country_name, capital FROM `country`");
+        $stmt = $conn->prepare("SELECT goal_id, scorer_id, asssister_id, game_id FROM `goal`");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
@@ -13,25 +13,25 @@ function selectGoals() {
     }
 }
 
-function selectGoalsForInput() {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT country_id, country_name FROM country order by country_name");
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $conn->close();
-        return $result;
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
-    }
-}
+//function selectGoalsForInput() {
+//    try {
+//        $conn = get_db_connection();
+//        $stmt = $conn->prepare("SELECT country_id, country_name FROM country order by country_name");
+//        $stmt->execute();
+  //      $result = $stmt->get_result();
+    //    $conn->close();
+      //  return $result;
+   // } catch (Exception $e) {
+     //   $conn->close();
+       // throw $e;
+    //}
+//}
 
-function insertGoal($cName, $capName) {
+function insertGoal($gScorer, $gAssister, $gGame) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("INSERT INTO `country` (`country_name`, `capital`) VALUES (?, ?)");
-        $stmt->bind_param("ss", $cName, $capName);
+        $stmt = $conn->prepare("INSERT INTO `goal` (`scorer_id`, `assister_id`, `game_id`) VALUES (?,?,?)");
+        $stmt->bind_param("iii", $gScorer, $gAssister, $gGame);
         $success=$stmt->execute();
         $conn->close();
         return $success;
@@ -41,11 +41,11 @@ function insertGoal($cName, $capName) {
     }
 }
 
-function updateGoal($cName, $capName, $cid) {
+function updateGoal($gScorer, $gAssister, $gGame, $gid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("UPDATE `country` set `country_name` = ?, `capital` = ? where country_id = ?");
-        $stmt->bind_param("ssi", $cName, $capName, $cid);
+        $stmt = $conn->prepare("UPDATE `goal` set `scorer_id` = ?, `assister_id` = ?, `game_id` = ? where game_id = ?");
+        $stmt->bind_param("iiii", $gScorer, $gAssister, $gGame, $gid);
         $success=$stmt->execute();
         $conn->close();
         return $success;
@@ -55,11 +55,11 @@ function updateGoal($cName, $capName, $cid) {
     }
 }
 
-function deleteGoal($cid) {
+function deleteGoal($gid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("delete from `country` where country_id = ?");
-        $stmt->bind_param("i", $cid);
+        $stmt = $conn->prepare("delete from `goal` where goal_id = ?");
+        $stmt->bind_param("i", $gid);
         $success=$stmt->execute();
         $conn->close();
         return $success;
