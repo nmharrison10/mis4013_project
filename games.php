83a63824f2a7ec50d9e0aa6b79626b5375aa127e
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-12-05 16:27:18 -0600

    Create games.php

---
+++ games.php
@@ -0,0 +1,41 @@
+<?php
+require_once("util-db.php");
+require_once("model-games.php");
+
+$pageTitle = "Games";
+include "view-header.php";
+
+if (isset($_POST['actionType'])){
+switch($_POST['actionType']){
+  case "Add":
+    if(insertGame($_POST['t1id'], $_POST['t1s'],$_POST['t2id'],,$_POST['t2s'],$_POST['sid'],$_POST['date'])) {
+  echo '<div style="margin:15px" class="alert alert-success" role="alert">Race Added</div>';
+    }
+    else{
+echo '<div class="alert alert-danger" role="alert">Error</div>"';
+    }
+  break;
+  case "Delete":
+    if(deleteGame($_POST['gid'])) {
+  echo '<div style="margin:15px" class="alert alert-success" role="alert">Game Deleted</div>';
+    }
+    else{
+echo '<div class="alert alert-danger" role="alert">Error</div>"';
+    }
+  break;
+  case "Edit":
+    if(updateGame($_POST['did'], $_POST['track'], $_POST['date'],$_POST['date'],$_POST['gid'])) {
+  echo '<div style="margin:15px" class="alert alert-success" role="alert">Game Edited</div>';
+    }
+    else{
+echo '<div class="alert alert-danger" role="alert">Error</div>"';
+    }
+  break;
+}
+}
+
+
+$games = selectGames();
+include "view-games.php";
+include "view-footer.php";
+?>
