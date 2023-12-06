b9f8478a381e823c55280035ec2a288757d4efbd
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-12-06 12:52:21 -0600

    Update view-footer.php

--- view-footer.php
+++ view-footer.php
@@ -1,4 +1,5 @@
 <html>
+<body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
     <span class="mb-3 mb-md-0 text-muted" style = "color:white;">Created by: Abby, Ashley, and Nicole</span>
    </div>
