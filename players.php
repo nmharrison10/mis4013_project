9519a7391c0d5533b01839ce26be85ea88bab6f6
AshleyMeza <111796979+AshleyMeza@users.noreply.github.com>
2023-12-06 13:14:45 -0600

    Update players.php

--- players.php
+++ players.php
@@ -1,4 +1,4 @@
- <?php
+<?php
 require_once ("util-db.php");
 require_once ("model-players.php");
 
@@ -7,9 +7,23 @@ include "view-header.php";
 
 if (isset($_POST['actionType'])){
  switch ($_POST['actionType']){
+  case 'Add':
+   if (insertPlayers($_POST['pName'],$_POST['pNumber'],$_POST['pAge'])){
+    echo '<div class="alert alert-success" role="alert"> Player Added!</div>';
+   } else {
+    echo '<div class="alert alert-danger" role="alert">Error</div>';
+   }
+   break;
   case 'Edit':
-   if (updatePlayers($_POST['pName'],$_POST['pAge'],$_POST['pid'])){
+   if (updatePlayers($_POST['pName'],$_POST['pNumber'],$_POST['pAge'],$_POST['pid'])){
     echo '<div class="alert alert-success" role="alert"> Player Updated!</div>';
+   } else {
+    echo '<div class="alert alert-danger" role="alert">Error</div>';
+   }
+   break;
+  case 'Delete':
+   if (deletePlayers($_POST['pid'])){
+    echo '<div class="alert alert-success" role="alert"> Player Deleted!</div>';
    } else {
     echo '<div class="alert alert-danger" role="alert">Error</div>';
    }
