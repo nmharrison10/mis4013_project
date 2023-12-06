16e56ef92db9a8d990f0290a674742a4074bb9b6
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-12-06 12:43:37 -0600

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,14 @@ include "view-header.php";
 ?>
 
 <h1>MIS4013 Project</h1>
+<style>
+body {
+  background-image: url('soccer.jpeg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+}
+</style>
 
 <?php
 include "view-footer.php";
