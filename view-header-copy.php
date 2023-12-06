9cf3f25ea2dfd207d735794c9dea0e65ef455ca9
AshleyMeza <111796979+AshleyMeza@users.noreply.github.com>
2023-12-06 12:33:19 -0600

    Create view-header-copy.php

---
+++ view-header-copy.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?=$pageTitle?></title>
+    <link rel = "stylesheet" href = "bootstrap.css">
+  </head>
+  <body>
+    <div class="container">
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        
+       <li class="nav-item">
+          <a class="nav-link" href="players.php">Players</a>
+        </li>
+
+         <li class="nav-item">
+            <a class="nav-link" href="clubs.php">Clubs</a>
+          </li>
+      
+        <li class="nav-item">
+          <a class="nav-link" href="countries.php">Countries</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="goals.php">Goals</a>
+        </li>
+        
+         <li class="nav-item">
+          <a class="nav-link" href="games.php">Games</a>
+         </li>
+        
+        <li class="nav-item">
+          <a class="nav-link" href="stadiums.php">Stadiums</a>
+        </li>
+          
+        <li class="nav-item">
+          <a class="nav-link" href="about-the-players.php">About the Players</a>
+        </li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
