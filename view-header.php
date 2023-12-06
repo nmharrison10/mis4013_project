2f716103d57945b80c6d50044bfeb3dbcaee714c
AshleyMeza <111796979+AshleyMeza@users.noreply.github.com>
2023-12-06 12:34:41 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -8,16 +8,15 @@
   </head>
   <body>
     <div class="container">
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <a class="navbar-brand" href="#" data-ol-has-click-handler="">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        
-        <li class="nav-item">
+    <div class="collapse navbar-collapse" id="navbarColor01">
+      <ul class="navbar-nav me-auto">
+           <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         
@@ -48,8 +47,12 @@
         <li class="nav-item">
           <a class="nav-link" href="about-the-players.php">About the Players</a>
         </li>
-        
+
       </ul>
+      <form class="d-flex">
+        <input class="form-control me-sm-2" type="search" placeholder="Search">
+        <button class="btn btn-secondary my-2 my-sm-0" type="submit" data-ol-has-click-handler="">Search</button>
+      </form>
     </div>
   </div>
 </nav>
