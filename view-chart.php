5a9c1934973756bbdf63a3df12c6ac79b52e2c65
AshleyMeza <111796979+AshleyMeza@users.noreply.github.com>
2023-12-06 11:14:19 -0600

    Create view-chart.php

---
+++ view-chart.php
@@ -0,0 +1,35 @@
+<script src="https://www.gstatic.com/charts/loader.js"></script>
+
+<h1>How many goals has each team scored in the FIFA World Cup?</h1>
+<body>
+<div
+id="myChart" style="width:100%; max-width:600px; height:500px;">
+</div>
+
+<script>
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+
+// Set Data
+const data = google.visualization.arrayToDataTable([
+  ['Contry', 'Mhl'],
+  ['Spain',108],
+  ['Germany',232],
+  ['England',104]
+]);
+
+// Set Options
+const options = {
+  title:'FIFA WC Goal Percentage'
+};
+
+// Draw
+const chart = new google.visualization.PieChart(document.getElementById('myChart'));
+chart.draw(data, options);
+
+}
+</script>
+
+</body>
