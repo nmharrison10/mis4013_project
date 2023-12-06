21e1777fbc7a28ee06d62247119253b7504497b3
AshleyMeza <111796979+AshleyMeza@users.noreply.github.com>
2023-12-06 13:16:14 -0600

    Update model-players.php

--- model-players.php
+++ model-players.php
@@ -12,11 +12,40 @@ function selectPlayers() {
         throw $e;
     }
 }
-function updatePlayers($pName,$pAge,$pid) {
+
+function insertPlayers($pName,$pNumber,$pAge) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("update `player` set `player_name`=?, `player_age`= ? where player_id=?");
-        $stmt->bind_param("sssi", $pName,$pAge,$pid);
+        $stmt = $conn->prepare("INSERT INTO `player` (`player_name`, `player_number`, `player_age`) VALUES (?, ?, ?)");
+        $stmt->bind_param("sss",$pName,$pNumber,$pAge);
+        $success = $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function updatePlayers($pName,$pNumber,$pAge,$pid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `player` set `player_name`=?, `player_number`= ?, `player_age`= ? where player_id=?");
+        $stmt->bind_param("sssi", $pName,$pNumber,$pAge,$pid);
+        $success = $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function deletePlayers($pid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from player where player_id = ?");
+        $stmt->bind_param("i", $pid);
         $success = $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
