46a6c96e5d815fce2e9fbaa6bee79e4d5683a61f
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-12-06 13:23:45 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -8,7 +8,7 @@
   </head>
   <body>
     <div class="container">
-      <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark" style="width:1920px;">
+      <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark" style="max-width:100%;">
   <div class="container-fluid">
     <a class="navbar-brand" href="#" data-ol-has-click-handler="">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
