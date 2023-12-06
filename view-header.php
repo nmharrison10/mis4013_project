d5d256a9b7f752e467b8ab79cf4e8e8735788985
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-12-06 13:20:37 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?=$pageTitle?></title>
     <link rel = "stylesheet" href = "bootstrap.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.3/leaflet.min.css" rel="stylesheet"/>              
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.3/leaflet.min.js"></script>
   </head>
   <body>
     <div class="container">
