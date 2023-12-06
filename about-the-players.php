49dc51e165f9f5ae7e6826958d7b324016720bf4
AshleyMeza <111796979+AshleyMeza@users.noreply.github.com>
2023-12-06 11:43:31 -0600

    Update about-the-players.php

--- about-the-players.php
+++ about-the-players.php
@@ -7,6 +7,7 @@ include "view-header.php";
 <div class="row">
   <div class = "col">
   <h1>"About the Players</h1>
+    <h5><a href="vew-chart.html">FIFA WC Goals</a></h5>
   </div>
   <div class = "col-auto">
   </div> 
@@ -20,8 +21,25 @@ include "view-header.php";
     <div class="col-md-8">
       <div class="card-body">
         <h5 class="card-title">Sergio Ramos</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+<p class = "card-text" style="text-align:left;">
+        83 PAC
+        <span style="float:right;">
+        77 DRI
+        </span>
+    </p>
+    <p class = "card-text" style="text-align:left;">
+        72 SHO
+        <span style="float:right;">
+        92 Def
+        </span>
+     </p>
+     <p class = "card-text" style="text-align:left;">
+        78 PAS
+        <span style="float:right;">
+        88 PHY
+        </span>
+    </p>
+        <p class="card-text"><small class="text-body-secondary">Current Club Team: Sevilla FC</small></p>
       </div>
     </div>
   </div>
@@ -35,8 +53,25 @@ include "view-header.php";
     <div class="col-md-8">
       <div class="card-body">
         <h5 class="card-title">Gavi</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+<p class = "card-text" style="text-align:left;">
+        76 PAC
+        <span style="float:right;">
+        83 DRI
+        </span>
+    </p>
+    <p class = "card-text" style="text-align:left;">
+        64 SHO
+        <span style="float:right;">
+        64 Def
+        </span>
+     </p>
+     <p class = "card-text" style="text-align:left;">
+        77 PAS
+        <span style="float:right;">
+        67 PHY
+        </span>
+    </p>
+        <p class="card-text"><small class="text-body-secondary">Current Club Team: FC Barcelona</small></p>
       </div>
     </div>
   </div>
@@ -50,8 +85,25 @@ include "view-header.php";
     <div class="col-md-8">
       <div class="card-body">
         <h5 class="card-title">Harry Kane</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+<p class = "card-text" style="text-align:left;">
+        82 PAC
+        <span style="float:right;">
+        85 DRI
+        </span>
+    </p>
+    <p class = "card-text" style="text-align:left;">
+        93 SHO
+        <span style="float:right;">
+        50 Def
+        </span>
+     </p>
+     <p class = "card-text" style="text-align:left;">
+        85 PAS
+        <span style="float:right;">
+        85 PHY
+        </span>
+    </p>       
+      <p class="card-text"><small class="text-body-secondary">Current Club Team: FC Bayern Munich</small></p>
       </div>
     </div>
   </div>
@@ -65,8 +117,25 @@ include "view-header.php";
     <div class="col-md-8">
       <div class="card-body">
         <h5 class="card-title">Jude Bellingham</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+<p class = "card-text" style="text-align:left;">
+        75 PAC
+        <span style="float:right;">
+        84 DRI
+        </span>
+    </p>
+    <p class = "card-text" style="text-align:left;">
+        75 SHO
+        <span style="float:right;">
+        77 Def
+        </span>
+     </p>
+     <p class = "card-text" style="text-align:left;">
+        78 PAS
+        <span style="float:right;">
+        80 PHY
+        </span>
+    </p>        
+        <p class="card-text"><small class="text-body-secondary">Current Club Team: Real Madrid CF</small></p>
       </div>
     </div>
   </div>
@@ -80,8 +149,25 @@ include "view-header.php";
     <div class="col-md-8">
       <div class="card-body">
         <h5 class="card-title">Kai Havertz</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+    <p class = "card-text" style="text-align:left;">
+        92 PAC
+        <span style="float:right;">
+        93 DRI
+        </span>
+    </p>
+    <p class = "card-text" style="text-align:left;">
+        90 SHO
+        <span style="float:right;">
+        52 Def
+        </span>
+     </p>
+     <p class = "card-text" style="text-align:left;">
+        88 PAS
+        <span style="float:right;">
+        86 PHY
+        </span>
+    </p>        
+    <p class="card-text"><small class="text-body-secondary">Current Club Team: Arsenal FC</small></p>
       </div>
     </div>
   </div>
@@ -90,17 +176,35 @@ include "view-header.php";
 <div class="card mb-3" style="max-width: 540px;">
   <div class="row g-0">
     <div class="col-md-4">
-      <img src="..." class="img-fluid rounded-start" alt="...">
+      <img src="..." class="img-fluid rounded-start" alt="center">
     </div>
     <div class="col-md-8">
       <div class="card-body">
         <h5 class="card-title">Niclas Fullkrug</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+    <p class = "card-text" style="text-align:left;">
+        77 PAC
+        <span style="float:right;">
+        85 DRI
+        </span>
+    </p>
+    <p class = "card-text" style="text-align:left;">
+        88 SHO
+        <span style="float:right;">
+        46 Def
+        </span>
+     </p>
+     <p class = "card-text" style="text-align:left;">
+        75 PAS
+        <span style="float:right;">
+        88 PHY
+        </span>
+    </p>
+        <p class="card-text"><small class="text-body-secondary">Current Club Team: Borussia Dortmund</small></p>
       </div>
     </div>
   </div>
 </div>
+
 <?php
 include "view-footer.php";
 ?>
