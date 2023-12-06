0f14bc37fb5e0d3ebf63fca13d01595a989deae3
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-12-06 11:50:55 -0600

    Update countries.php

--- countries.php
+++ countries.php
@@ -6,13 +6,13 @@ $pageTitle = "Countries";
 include "view-header.php";
 ?>
 
-  <div id="carouselExampleCaptions" class="carousel slide" style="height:100px;">
+  <div id="carouselExampleCaptions" class="carousel slide">
   <div class="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
-  <div class="carousel-inner">
+  <div class="carousel-inner" style="height:100px;">
     <div class="carousel-item active">
       <img src="spain.png" class="d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
