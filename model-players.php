3c780fdbd7adabf4564d3ed69246f050212b6c37
AshleyMeza <111796979+AshleyMeza@users.noreply.github.com>
2023-12-09 13:44:28 -0600

    Update model-players.php

--- model-players.php
+++ model-players.php
@@ -13,6 +13,20 @@ function selectPlayers() {
     }
 }
 
+function selectPlayersForInput() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT player_id, player_name, player_age FROM player order by player_name");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
 function insertPlayers($pName,$pNumber,$pAge) {
     try {
         $conn = get_db_connection();
