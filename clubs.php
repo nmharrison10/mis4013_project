4d5f9661b1026f755b652990b562a19ecc423d89
AshleyMeza <111796979+AshleyMeza@users.noreply.github.com>
2023-12-09 13:08:32 -0600

    Update clubs.php

--- clubs.php
+++ clubs.php
@@ -8,21 +8,21 @@ include "view-header.php";
 if (isset($_POST['actionType'])){
  switch ($_POST['actionType']){
   case 'Add':
-   if (insertClubs($_POST['cName'],$_POST['cCoach'],$_POST['cLocation'])){
+   if (insertClubs($_POST['clName'],$_POST['clCoach'],$_POST['clLocation'])){
     echo '<div class="alert alert-success" role="alert"> New Club Added!</div>';
    } else {
     echo '<div class="alert alert-danger" role="alert">Error</div>';
    }
    break;
   case 'Edit':
-   if (updateClubs($_POST['cName'],$_POST['cCoach'],$_POST['cLocation'],$_POST['cid'])){
+   if (updateClubs($_POST['clName'],$_POST['clCoach'],$_POST['clLocation'],$_POST['clid'])){
     echo '<div class="alert alert-success" role="alert"> Club Updated!</div>';
    } else {
     echo '<div class="alert alert-danger" role="alert">Error</div>';
    }
    break;
   case 'Delete':
-   if (deleteClubs($_POST['cid'])){
+   if (deleteClubs($_POST['clid'])){
     echo '<div class="alert alert-success" role="alert"> Club Deleted!</div>';
    } else {
     echo '<div class="alert alert-danger" role="alert">Error</div>';
