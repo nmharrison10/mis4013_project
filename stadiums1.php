f572909f24afe74b80ffab3997b1cfa56f812171
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-12-12 17:23:30 -0600

    Create stadiums1.php

---
+++ stadiums1.php
@@ -0,0 +1,40 @@
+<?php
+require_once("util-db.php");
+require_once("model-stadiums1.php");
+
+$pageTitle = "Stadiums";
+include "view-header.php";
+
+if (isset($_POST['actionType'])){
+switch($_POST['actionType']){
+  case "Add";  
+  if(insertStadium($_POST['sName'],$_POST['sLong'],$_POST['sLat'],$_POST['sLoc'])) {
+  echo '<div style="margin:15px" class="alert alert-success" role="alert">Stadium Added</div>';
+    }
+    else{
+echo '<div class="alert alert-danger" role="alert">Error</div>"';
+    }
+  break;
+  case "Delete":
+    if(deleteStadium($_POST['sid'])) {
+  echo '<div style="margin:15px" class="alert alert-success" role="alert">Stadium Deleted</div>';
+    }
+    else{
+echo '<div class="alert alert-danger" role="alert">Error</div>"';
+    }
+  break;
+  case "Edit":
+    if(updateStadium($_POST['sid'], $_POST['sName'], $_POST['sLat'], $_POST['sLong'], $_POST['sLoc'])) {
+  echo '<div style="margin:15px" class="alert alert-success" role="alert">Stadium Edited</div>';
+    }
+    else{
+echo '<div class="alert alert-danger" role="alert">Error</div>"';
+    }
+  break;
+}
+}
+
+$stadiums = selectStadiums();
+include "view-stadiums.php";
+include "view-footer.php";
+?>
