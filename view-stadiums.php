e1fe78c0de35a3b45757041b27df31c07fa78c71
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-12-12 17:38:39 -0600

    Update view-stadiums.php

--- view-stadiums.php
+++ view-stadiums.php
@@ -3,6 +3,9 @@
 <h1>Stadiums</h1>
   </div>
   <div class="col-auto">
+<?php
+include "view-stadiums-newform.php";
+?>
   </div>
 </div>
 <div class="table-responsive">
@@ -29,7 +32,9 @@ while ($stadium = $stadiums->fetch_assoc()) {
   <td><?php echo $stadium['stadium_long']?></td>
   <td><?php echo $stadium['location']?></td>
   <td>
-    
+<?php
+include "view-stadiums-editform.php";
+?>
   </td>
   <td>
     <form method="post" action="">
