15bcbc3aed343cdde3679d679e8947de91ed4988
AshleyMeza <111796979+AshleyMeza@users.noreply.github.com>
2023-12-12 22:17:53 -0600

    Create model-clubs-by-country.php

---
+++ model-clubs-by-country.php
@@ -0,0 +1,16 @@
+<?php
+function selectClubsbyCountry($cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT club_id,club_name,coach,location,country_name FROM `club` cl JOIN country c ON c.club_id = c.country_id WHERE c.country_id = ?");
+        $stmt->bind_param("i",$cid); 
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
