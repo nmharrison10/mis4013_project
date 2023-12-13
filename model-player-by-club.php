fc9f8226f952be2052f810fb02bb4dd890a86d74
AshleyMeza <111796979+AshleyMeza@users.noreply.github.com>
2023-12-13 11:49:27 -0600

    Update model-player-by-club.php

--- model-player-by-club.php
+++ model-player-by-club.php
@@ -2,7 +2,7 @@
 function selectPlayersbyClub($clid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT player_id,player_name,player_number,player_age,club_name FROM `player` p JOIN club c ON p.club_id = c.club_id WHERE p.club_id = ?");
+        $stmt = $conn->prepare("SELECT player_id,player_name,player_number,player_age,club_id, club_name FROM `player` p JOIN club c ON p.club_id = c.club_id WHERE p.club_id = ?");
         $stmt->bind_param("i",$clid); 
         $stmt->execute();
         $result = $stmt->get_result();
