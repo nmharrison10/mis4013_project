850c48fa9759054bf864de0455eebcc947170524
AshleyMeza <111796979+AshleyMeza@users.noreply.github.com>
2023-12-12 22:22:29 -0600

    Create player-by-club.php

---
+++ player-by-club.php
@@ -0,0 +1,12 @@
+ <?php
+require_once ("util-db.php");
+require_once ("model-player-by-club.php");
+
+$pageTitle = "Club Roster";
+include "view-header.php";
+
+$clubs = selectPlayersbyClub($_POST['clid']);
+include "view-player-by-club.php";
+
+include "view-footer.php";
+?>
