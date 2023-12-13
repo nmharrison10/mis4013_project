d4f5cacd62d3d585fc9760ec127bc013d515c6f7
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-12-13 16:14:33 -0600

    Update model-players.php

--- model-players.php
+++ model-players.php
@@ -13,19 +13,19 @@ function selectPlayers() {
     }
 }
 
-//function selectPlayersForInput() {
-   // try {
-     //   $conn = get_db_connection();
-       // $stmt = $conn->prepare("SELECT club_id, club_name, coach, location FROM club order by club_name");
-        //$stmt->execute();
- //       $result = $stmt->get_result();
-  //      $conn->close();
- //       return $result;
-//    } catch (Exception $e) {
-//        $conn->close();
-  //      throw $e;
- //   }
-//}
+function selectClubsForInput() {
+try {
+$conn = get_db_connection();
+$stmt = $conn->prepare("SELECT club_id, club_name, coach, location FROM club order by club_name");
+$stmt->execute();
+$result = $stmt->get_result();
+$conn->close();
+ return $result;
+ } catch (Exception $e) {
+$conn->close();
+throw $e;
+ }
+}
 
 function insertPlayers($pName,$pNumber,$pAge) {
     try {
