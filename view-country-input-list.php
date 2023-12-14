4bd275f0565fb319a84326d689b4528ce537af05
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-12-14 14:34:13 -0600

    Update view-country-input-list.php

--- view-country-input-list.php
+++ view-country-input-list.php
@@ -1,10 +1,9 @@
-<select class="form-select" id="cid" name="cid">
+<select class="form-select" id="countryid" name="countryid">
   <?php
     while ($countrynameItem =$countryList->fetch_assoc()){
       $selText = "";
       if ($selectedCountryname == $countrynameItem['country_id']){
         $selText = "selected";
-
       }
       ?>
       <option value="<?php echo $countrynameItem['country_id'];?>"<?=$selText?>><?php echo $countrynameItem['country_name'];?></option>
