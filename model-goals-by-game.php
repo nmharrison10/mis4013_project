9de89c6ff674fb734c86b7ac1298f61ccf6c591f
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-12-14 17:18:20 -0600

    Update model-goals-by-game.php

--- model-goals-by-game.php
+++ model-goals-by-game.php
@@ -14,4 +14,48 @@ function selectGoalsByGame($gameid) {
         throw $e;
     }
 }
+
+
+function insertGoalByGame($gScorer, $gAssister, $gGame) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `goal` (`scorer_id`, `assister_id`, `game_id`) VALUES (?,?,?)");
+        $stmt->bind_param("iii", $gScorer, $gAssister, $gGame);
+        $success=$stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateGoalByGame($gScorer, $gAssister, $gGame, $gid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `goal` set `scorer_id` = ?, `assister_id` = ?, `game_id` = ? where game_id = ?");
+        $stmt->bind_param("iiii", $gScorer, $gAssister, $gGame, $gid);
+        $success=$stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteGoal($gid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from `goal` where goal_id = ?");
+        $stmt->bind_param("i", $gid);
+        $success=$stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
 ?>
