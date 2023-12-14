46dfaa3d6e9a9a67cf6d833bb93775cb0e937897
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-12-14 16:30:56 -0600

    Update view-players-newform.php

--- view-players-newform.php
+++ view-players-newform.php
@@ -38,9 +38,11 @@
             </div>
           <div class="mb-3">
               <label for="countryid" class="form-label">Player's Country</label>
-            <input type="text" class="form-control" id="countryid" name = "countryid">
-           
-            </div>
+<?php
+    $countryList = selectCountriesForInput();
+    $selectedCountryname = "";
+    include "view-country-input-list.php";
+?> </div>
                 <input type = "hidden" name = "actionType" value = "Add"> 
             <button type="submit" class="btn btn-primary">Save</button>
           </form>
