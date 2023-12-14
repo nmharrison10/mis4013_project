695532c554fdf7b1412cdfbe278567b1fa9d3055
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-12-14 16:53:45 -0600

    Create view-goals-by-game.php

---
+++ view-goals-by-game.php
@@ -0,0 +1,33 @@
+<h1>Goals By Game</h1>
+<div class="table-responsive">
+  <table class="table">
+    <head>
+      <tr>
+      <th>Game ID</th>
+      <th>Goal ID</th>
+      <th>Scorer ID</th>
+      <th>Scorer</th>
+      <th>Assister ID</th>
+      <th>Assister</th>
+      </tr>
+    </head>
+    <body>
+<?php
+    while ($goal = $goals->fetch_assoc()) {
+?>
+<tr>
+      <td><?php echo $goal['game_id']?></td>
+      <td><?php echo $goal['goal_id']?></td>
+      <td><?php echo $goal['scorer_id']?></td>
+      <td><?php echo $goal['scorer']?></td>
+      <td><?php echo $goal['assister_id']?></td>
+      <td><?php echo $goal['assister']?></td>
+</tr>
+<?php
+}
+?>
+    </body>
+  </table>
+</div>
+<?php
+?>
