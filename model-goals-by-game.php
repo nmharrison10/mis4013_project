cb52d63631472d42393357abea1ab80480061024
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-12-14 16:59:52 -0600

    Update model-goals-by-game.php

--- model-goals-by-game.php
+++ model-goals-by-game.php
@@ -1,10 +1,10 @@
 <?php
-function selectGoalsByGame($gameid) {
+function selectGoalsByGame($gid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("select `goal_id`,`scorer_id`,`scorer`.`player_name` as scorer, `assister_id`, `assister`.`player_name` as assister, `game_id` from `goal` 
         join `player` scorer on goal.scorer_id=scorer.player_id left join `player` assister on goal.assister_id=assister.player_id where `game_id` = ?");
-        $stmt->bind_param("i", $gameid);
+        $stmt->bind_param("i", $gid);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
