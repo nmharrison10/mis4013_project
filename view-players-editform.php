430787d586217406a8794b42fd500787c6d3c7ef
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-12-14 14:21:02 -0600

    Update view-players-editform.php

--- view-players-editform.php
+++ view-players-editform.php
@@ -7,47 +7,46 @@
 </button>
 
 <!-- Modal -->
-<div class="modal fade" id="editPlayerModal<?php echo $player['player_id'];?>" tabindex="-1" aria-labelledby="editPlayerModelLabel<?php echo $player['player_id'];?>" aria-hidden="true">
+<div class="modal fade" id="editPlayerModal<?php echo $player['player_id'];?>" tabindex="-1" aria-labelledby="editPlayerModalLabel<?php echo $player['player_id'];?>" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="editPlayerModal<?php echo $player['player_id']; ?>">Edit Player</h1>
+        <h1 class="modal-title fs-5" id="editPlayerModalLabel<?php echo $player['player_id'];?>">Edit Player</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <form method = "post" action="">
-            <div class="mb-3">
-              <label for="pName<?php echo $player['player_id']; ?>" class="form-label">Player's Name</label>
-              <input type="text" class="form-control" id="pName<?php echo $player['player_id']; ?>" name = "pName" value = "<?php echo $player['player_name'];?>">
-            </div>
+            <form method="post" action="">
           <div class="mb-3">
-              <label for="pNumber<?php echo $player['player_id']; ?>" class="form-label">Player's Number</label>
-              <input type="text" class="form-control" id="pNumber<?php echo $player['player_id']; ?>" name = "pNumber" value = "<?php echo $player['player_number']; ?>">
-            </div>
-          
-            <div class="mb-3">
-              <label for="pAge<?php echo $player['player_id']; ?>" class="form-label">Player's Age</label>
-              <input type="text" class="form-control" id="pAge<?php echo $player['player_id']; ?>" name = "pAge" value = "<?php echo $player['player_age']; ?>">
-            </div>
-          
+            <label for="pName<?php echo $player['player_id'];?>" class="form-label">Player Name</label>
+            <input type="text" class="form-control" id="pName<?php echo $player['player_id'];?>" name="pName" value="<?php echo $player['player_name'];?>">
+          </div>
           <div class="mb-3">
-              <label for="cid<?php echo $player['player_id']; ?>" class="form-label">Player's Club</label>
+            <label for="pNumber<?php echo $player['player_id'];?>" class="form-label">Player Number</label>
+            <input type="text" class="form-control" id="pNumber<?php echo $player['player_id'];?>" name="pNumber" value="<?php echo $player['player_number'];?>">
+          </div>
+        <div class="mb-3">
+            <label for="pAge<?php echo $player['player_id'];?>" class="form-label">Player Age</label>
+            <input type="text" class="form-control" id="pAge<?php echo $player['player_id'];?>" name="pAge" value="<?php echo $player['player_age'];?>">
+          </div>
+
+          <div class="mb-3">
+            <label for="cid<?php echo $player['player_id'];?>" class="form-label">Club</label>
 <?php
     $clubList = selectClubsForInput();
-    $selectedClubname = $player['club_id'];
+    $selectedClubname = $player['club_id'];;
     include "view-club-input-list.php";
-?>
-            </div>
-          
-          <div class="mb-3">
-              <label for="countryid<?php echo $player['player_id']; ?>" class="form-label">Player's Country</label>
-              <input type="text" class="form-control" id="countryid<?php echo $player['player_id']; ?>" name = "countryid" value = "<?php echo $player['country_id']; ?>">
-            </div>
-          
-                <input type="hidden" name = "pid" value = "<?php echo $player['player_id']; ?>">
-                <input type = "hidden" name = "actionType" value = "Edit"> 
-            <button type="submit" class="btn btn-primary">Save</button>
-          </form>
+?></div>
+        
+        <div class="mb-3">
+            <label for="countryid<?php echo $player['player_id'];?>" class="form-label">Country</label>
+            <input type="text" class="form-control" id="countryid<?php echo $player['player_id'];?>" name="countryid" value="<?php echo $player['country_id'];?>">
+          </div>
+              
+        <input type="hidden" name="pid" value="<?php echo $player['player_id'];?>">
+          <input type="hidden" name="actionType" value="Edit">
+          <button type="submit" class="btn btn-primary">Save</button>
+            </form>
+        
       </div>
     </div>
   </div>
