f008fd7cfe1fbfaa8c73ddb263199e811ec0287b
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-12-14 15:17:04 -0600

    Update view-stadiums.php

--- view-stadiums.php
+++ view-stadiums.php
@@ -27,7 +27,7 @@ include "view-stadiums-newform.php";
 <?php
 while ($stadium = $stadiums->fetch_assoc()) {
 ?>
-<tr class="table-primary">
+<tr>
   <td><?php echo $stadium['stadium_id']?></td>
   <td><?php echo $stadium['stadium_name']?></td>
   <td><?php echo $stadium['stadium_lat']?></td>
