f58a8a212ad58471c1c2b4c3704a702a538ad51a
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-12-14 16:02:15 -0600

    Update players.php

--- players.php
+++ players.php
@@ -9,23 +9,23 @@ if (isset($_POST['actionType'])){
  switch ($_POST['actionType']){
   case 'Add':
    if (insertPlayers($_POST['pName'],$_POST['pNumber'],$_POST['pAge'],$_POST['cid'],$_POST['countryid'])){
-    echo '<div class="alert alert-success" role="alert">Player Added!</div>';
+    echo '<div style="margin:15px" class="alert alert-success" role="alert">Player Added!</div>';
    } else {
-    echo '<div class="alert alert-danger" role="alert">Error</div>';
+    echo '<div style="margin:15px" class="alert alert-danger" role="alert">Error</div>';
    }
    break;
   case 'Edit':
    if (updatePlayers($_POST['pName'],$_POST['pNumber'],$_POST['pAge'],$_POST['cid'],$_POST['countryid'],$_POST['pid'] )){
-    echo '<div class="alert alert-success" role="alert">Player Updated!</div>';
+    echo '<div style="margin:15px" class="alert alert-success" role="alert">Player Updated!</div>';
    } else {
-    echo '<div class="alert alert-danger" role="alert">Error</div>';
+    echo '<div style="margin:15px" class="alert alert-danger" role="alert">Error</div>';
    }
    break;
   case 'Delete':
    if (deletePlayers($_POST['pid'])){
-    echo '<div class="alert alert-success" role="alert"> Player Deleted!</div>';
+    echo '<div style="margin:15px" class="alert alert-success" role="alert"> Player Deleted!</div>';
    } else {
-    echo '<div class="alert alert-danger" role="alert">Error</div>';
+    echo '<div style="margin:15px" class="alert alert-danger" role="alert">Error</div>';
    }
    break;
  }
