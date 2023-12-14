d6533ec01a93cb3cab305ca484ba4cef477a0e64
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-12-14 17:11:06 -0600

    Update goals-by-game.php

--- goals-by-game.php
+++ goals-by-game.php
@@ -5,35 +5,6 @@ require_once("model-goals-by-game.php");
 $pageTitle = "Goals by Game";
 include "view-header.php";
 
-if (isset($_POST['actionType'])){
-switch($_POST['actionType']){
-  case "Add":
-    if(insertGoal($_POST['gScorer'], $_POST['gAssister'], $_POST['gGame'])) {
-  echo '<div style="margin:15px" class="alert alert-success" role="alert">Added Goal</div>';
-    }
-    else{
-echo '<div class="alert alert-danger" role="alert">Error</div>"';
-    }
-  break;
-  case "Delete":
-    if(deleteGoal($_POST['gid'])) {
-  echo '<div style="margin:15px" class="alert alert-success" role="alert">Deleted Goal</div>';
-    }
-    else{
-echo '<div class="alert alert-danger" role="alert">Error</div>"';
-    }
-  break;
-  case "Edit":
-    if(updateGoal($_POST['gScorer'], $_POST['gAssister'], $_POST['gGame'], $_POST['gid'])) {
-  echo '<div style="margin:15px" class="alert alert-success" role="alert">Edited Goal</div>';
-    }
-    else{
-echo '<div class="alert alert-danger" role="alert">Error</div>"';
-    }
-  break;
-}
-}
-
 
 $goals = selectGoalsByGame($_POST['gameid']);
 include "view-goals-by-game.php";
