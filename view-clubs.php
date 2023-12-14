679ec47f63b942cd349aec4233e6561450c94caa
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-12-14 15:15:03 -0600

    Update view-clubs.php

--- view-clubs.php
+++ view-clubs.php
@@ -10,7 +10,7 @@ include "view-clubs-newform.php";
 </div>
 
 <div class="table-responsive">
-  <table class="table">
+  <table class="table table-primary">
     <thead>
       <tr>
       <th>Club ID</th>
@@ -25,7 +25,7 @@ include "view-clubs-newform.php";
   <?php
   while ($club = $clubs->fetch_assoc()) {
     ?>
-    <tr class="table-primary">
+    <tr>
       <td><?php echo $club['club_id'];?></td>
       <td><?php echo $club['club_name'];?></td>
       <td><?php echo $club['coach'];?></td>
