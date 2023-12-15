a93320a74c6d681de72e90001e298396e820eb91
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-12-15 08:51:23 -0600

    Update games-chart.php

--- games-chart.php
+++ games-chart.php
@@ -1,7 +1,7 @@
 <?php
 require_once("util-db.php");
 require_once("model-games-chart.php");
-<script src="path/to/chartjs/dist/chart.umd.js"></script>
+
 $pageTitle = "Goals per Club";
 include "view-header.php";
 
