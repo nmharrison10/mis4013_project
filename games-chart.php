b61cee475c7d045fe09da1e9c608e4bb48509969
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-12-15 08:59:35 -0600

    Update games-chart.php

--- games-chart.php
+++ games-chart.php
@@ -1,8 +1,11 @@
 <?php
 require_once("util-db.php");
+$pageTitle = "Games";
+include "view-header.php";
 require_once("model-games-chart.php");
 
 
 $games = selectGames();
 include "view-games-chart.php";
+include "view-footer.php";
 ?>
