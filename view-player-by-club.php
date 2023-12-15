90ea396c15c69cfb9bb529e05ec6f056756e1ee6
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-12-14 20:44:05 -0600

    Update view-player-by-club.php

--- view-player-by-club.php
+++ view-player-by-club.php
@@ -1,25 +1,31 @@
 <h1>Club Roster</h1>
 <div class="table-responsive">
   <table class="table">
-    <thead>
+    <head>
       <tr>
       <th>Club Name</th>
       <th>Player ID</th>
       <th>Player Name</th>
-      <th>Number</th>
-      <th>Age</th>
+      <th>Player Number</th>
+      <th>Player Age</th>
       </tr>
-      <tbody>
-  <?php
-  while ($player = $players -> fetch_assoc()) {
-    ?>
-    <tr>
-      <td><?php echo $player['club_name'];?></td>
-      <td><?php echo $player['player_id'];?></td>
-      <td><?php echo $player['player_name'];?></td>
-      <td><?php echo $player['player_number'];?></td>
-      <td><?php echo $player['player_age'];?></td>
-      </tbody>
-    </thead>
+    </head>
+    <body>
+<?php
+    while ($player = $players->fetch_assoc()) {
+?>
+<tr>
+      <td><?php echo $player['club_name']?></td>
+      <td><?php echo $player['player_id']?></td>
+      <td><?php echo $player['player_name']?></td>
+      <td><?php echo $player['player_number']?></td>
+      <td><?php echo $player['player_age']?></td>
+</tr>
+<?php
+}
+?>
+    </body>
   </table>
 </div>
+<?php
+?>
