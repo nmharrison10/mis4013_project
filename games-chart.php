a75650d267f88252b8edb8759aaa2ff2d3182384
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-12-15 08:55:46 -0600

    Update games-chart.php

--- games-chart.php
+++ games-chart.php
@@ -2,12 +2,7 @@
 require_once("util-db.php");
 require_once("model-games-chart.php");
 
-$pageTitle = "Goals per Club";
-include "view-header.php";
-
-
 
 $games = selectGames();
 include "view-games-chart.php";
-include "view-footer.php";
 ?>
