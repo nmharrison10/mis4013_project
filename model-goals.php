f52ae14e662823cd127aee19b354d371a09696bc
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-12-14 20:03:29 -0600

    Update model-goals.php

--- model-goals.php
+++ model-goals.php
@@ -45,7 +45,7 @@ function insertGoal($gScorer, $gAssister, $gGame) {
 function updateGoal($gScorer, $gAssister, $gGame, $gid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("UPDATE `goal` set `scorer_id` = ?, `assister_id` = ?, `game_id` = ? where game_id = ?");
+        $stmt = $conn->prepare("UPDATE `goal` set `scorer_id` = ?, `assister_id` = ?, `game_id` = ? where goal_id = ?");
         $stmt->bind_param("iiii", $gScorer, $gAssister, $gGame, $gid);
         $success=$stmt->execute();
         $conn->close();
