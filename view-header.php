d6862c95bf29404250ca0eab56c8c4ec0283b9ad
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-12-14 20:49:49 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -14,7 +14,7 @@
     <div class="container">
       <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark" style="width:1920px;">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#" data-ol-has-click-handler="">Navbar</a>
+    <a class="navbar-brand" href="#" data-ol-has-click-handler="">Soccer</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
