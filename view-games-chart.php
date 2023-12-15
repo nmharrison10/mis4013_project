31ff354e14558d92af3aa08e46f7a45d2028d9c5
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-12-15 08:43:45 -0600

    Create view-games-chart.php

---
+++ view-games-chart.php
@@ -0,0 +1,40 @@
+ <h1>Games</h1>
+<div>
+  <canvas id="myChart"></canvas>
+</div>
+
+<script src="path/to/chartjs/dist/chart.umd.js"></script>
+<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+    datasets: [{
+        data: [<?php
+while ($game = $games->fetch_assoc()) {
+  echo $game['goals'].", ";
+}
+?>]
+    }],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+     <?php
+$games = SelectGames();
+while ($game = $games->fetch_assoc()) {
+  echo "'"$game['club_name']."', ";
+}
+?>
+]
+},
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+</script>
+ 
