ae25d7955feae6d0c4e42b274be192cef72a68ec
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-12-14 20:40:06 -0600

    Update view-player-by-club.php

--- view-player-by-club.php
+++ view-player-by-club.php
@@ -3,36 +3,22 @@
   <table class="table">
     <thead>
       <tr>
+      <th>Club Name</th>
       <th>Player ID</th>
       <th>Player Name</th>
       <th>Number</th>
       <th>Age</th>
-      <th>Club ID</th>
-      <th>Club Name</th>
       </tr>
       <tbody>
   <?php
   while ($player = $players -> fetch_assoc()) {
     ?>
     <tr>
+      <td><?php echo $player['club_name'];?></td>
       <td><?php echo $player['player_id'];?></td>
       <td><?php echo $player['player_name'];?></td>
       <td><?php echo $player['player_number'];?></td>
       <td><?php echo $player['player_age'];?></td>
-    </tr>
-    <?php
-  }
-  ?>
- <?php
-  while ($club = $clubs -> fetch_assoc()) {
-    ?>
-    <tr>
-      <td><?php echo $club['club_id'];?></td>
-      <td><?php echo $club['club_name'];?></td>
-    </tr>
-    <?php
-  }
-  ?>
       </tbody>
     </thead>
   </table>
